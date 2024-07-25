4fca9e081fa8c5e6265c7b699d47c70af7e2a75f
jimmyale3102 <jimmyale3102@gmail.com>
2024-07-25 11:45:30 +1000

    Initial Commit

---
+++ SuperHeroFinder/SuperHeroFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroFinderApp.swift
+//  SuperHeroFinder
+//
+//  Created by Jimmy on 25/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
